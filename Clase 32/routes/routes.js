4edae73e82aad4ff75fd0ba45d09754a9a048835
fedekolo <fedekolonski@gmail.com>
2021-12-08 20:29:31 -0300

    ejer 32 con errores

---
+++ Clase 32/routes/routes.js
@@ -0,0 +1,146 @@
+// CONFIG INICIAL
+const express = require('express');
+const router = express.Router();
+const routerApi = express.Router();
+const app = express();
+const conexionDB = require('../db/conexionDB');
+const Producto = require('../models/productos');
+const Archivo = require('../controller/productos');
+const {usuarios} = require('../controller/passport');
+// const passport = require('passport');
+const {fork} = require('child_process');
+const numCPUs = require('os').cpus().length;
+const log4js = require('log4js');
+const loggerError = log4js.getLogger('error');
+const loggerConsola = log4js.getLogger('info');
+
+// CONFIG USERS
+const auth = (req, res, next) => {
+    if (req.session?.admin) {
+        return next();
+    } else {
+        return res.sendStatus(401);
+    }
+};
+
+// RUTAS
+router.get('/',(req,res) => {
+    res.redirect('/api/productos/agregar');
+});
+
+routerApi.get('/productos/agregar', async (req,res) => {
+    try {
+        conexionDB();
+        const bdProductos = await Producto.find().lean();
+        const bd = new Archivo(bdProductos);
+        const archivo = await bd.listar();
+        const sinProductos = archivo.length==0 ? true : false;
+        res.render('add',{producto: archivo,sinProductos: sinProductos, admin: req.session?.admin, user: req.session?.user});
+        
+    } catch (err) {
+        loggerError.error(err);
+    }
+});
+
+routerApi.get('/productos/vista-test', auth, async (req,res) => {
+    try {
+        const faker = require('./faker/generador');
+        const bdProductos = []
+        for (let index = 0; index < 10; index++) {
+            bdProductos.push(faker.generadorInfo());
+        }
+        const bd = new Archivo(bdProductos);
+        const archivo = await bd.listar();
+        const sinProductos = archivo.length==0 ? true : false;
+        res.render('add',{producto: archivo,sinProductos: sinProductos});
+    } catch (err) {
+        loggerError.error(err);
+    }
+});
+
+routerApi.get('/productos/vista-test/:cant', async (req,res) => {
+    try {
+        let cant = req.params;
+        const faker = require('./faker/generador');
+        const bdProductos = []
+        for (let index = 0; index < cant.cant; index++) {
+            bdProductos.push(faker.generadorInfo());
+        }
+        const bd = new Archivo(bdProductos);
+        const archivo = await bd.listar();
+        const sinProductos = archivo.length==0 ? true : false;
+        res.render('add',{producto: archivo,sinProductos: sinProductos});
+    } catch (err) {
+        loggerError.error(err);
+    }
+});
+
+routerApi.post('/productos/guardar', async (req,res) => {
+    try {
+        conexionDB();
+        const productoBody = req.body;
+        const bdProductos = await Producto.find().lean();
+        const bd = new Archivo(bdProductos);
+        await bd.guardar(productoBody);
+        res.redirect('/api/productos/agregar');
+    }
+    catch (err) {
+        loggerError.error(err);
+    }
+});
+
+routerApi.put('/productos/actualizar/:id', async (req,res) => {
+    try {
+        conexionDB();
+        const params = req.params;
+        const productoBody = req.body;
+        const bdProductos = await Producto.find().lean();
+        const bd = new Archivo(bdProductos);
+        const archivo = await bd.actualizar(params.id,productoBody);
+        res.json(archivo);
+    } catch (err) {
+        loggerError.error(err);
+    }
+});
+
+routerApi.delete('/productos/borrar/:id', async (req,res) => {
+    try {
+        conexionDB();
+        const params = req.params;
+        const bdProductos = await Producto.find().lean();
+        const bd = new Archivo(bdProductos);
+        const archivo = await bd.borrar(params.id);
+        res.json(archivo);
+    }
+    catch (err) {
+        loggerError.error(err);
+    }
+});
+
+router.get('/info', (req,res) => {
+    const info = {
+        argumentos: process.argv,
+        sistemaOperativo: process.platform,
+        versionNode: process.version,
+        usoMemoria: process.memoryUsage().heapUsed,
+        pathEjecucion: process.execPath,
+        processId: process.pid,
+        carpeta: process.cwd(),
+        procesadores: `La cantidad de procesadores es ${numCPUs}`
+    };
+    console.log(info);
+    res.json(info);
+});
+
+router.get('/randoms', (req,res) => {
+    let {cant} = req.query;
+    if (cant == undefined) {
+        cant = '100000000';
+    }
+    // const numerosRandom = fork('./controller/numerosRandom.js');
+    // numerosRandom.send(cant);
+    // numerosRandom.on('message',numero=>res.end(`${numero}`));
+    loggerConsola.info("Es no bloqueante");
+});
+
+module.exports = { router, routerApi, app };
---
+++ Clase 32/server.js
@@ -0,0 +1,146 @@
+// INICIADORES
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const flash = require('express-flash');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+// const {normalize, schema} = require('normalizr');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+const router = require('./routes/routes');
+const {user} = require('./controller/passport');
+const {fork} = require('child_process');
+const cluster = require('cluster');
+const compression = require('compression');
+const log4js = require('log4js');
+const loggerConsola = log4js.getLogger('info');
+const loggerWarning = log4js.getLogger('warning');
+const loggerError = log4js.getLogger('error');
+
+// MIDDLEWARES
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://fedekolo:hLpilX2lYVbZmMag@cluster0.gsxpw.mongodb.net/cluster0?retryWrites=true&w=majority',
+        mongoOptions: advancedOptions
+    }),
+    secret: 'secreto',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api',router.routerApi);
+app.use('/',router.router);
+app.use('/user',user);
+// app.use(express.static('views'));
+
+//Para convertir en HTTPS
+const https = require('https');
+const fs = require('fs'); 
+const httpsOptions = {
+    key: fs.readFileSync('./sslcert/cert.key'),
+    cert: fs.readFileSync('./sslcert/cert.pem')
+};
+
+// SERVIDOR / ENRUTADOR / CLUSTER
+const PORT = parseInt(process.argv[2] || 8081);
+const modoCluster = process.argv[3] == 'CLUSTER';
+
+if (cluster.isMaster && modoCluster){
+    loggerConsola.info(`Master PID ${process.pid} está corriendo`);
+    cluster.fork();
+    cluster.on('exit', (worker, code, signal) => { 
+        loggerWarning.warning(`Worker ${worker.process.pid} finalizó`);
+        cluster.fork();
+    });
+} else {
+    const server = https.createServer(httpsOptions, app)
+    .listen(PORT, () => {
+        loggerConsola.info('Server corriendo en ' + PORT);
+    });
+    server.on('error', error=>loggerError.error(error));
+};
+
+// CONFIG HANDLEBARS
+app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+        defaultLayout: "main.hbs",
+        layoutsDir: __dirname + "/views"
+    })
+);
+
+app.set('views', './views'); 
+app.set('view engine', 'hbs');
+
+// CONFIG LOG4JS
+log4js.configure({
+    appenders: {
+        consola: { type: "console" },
+        fileWarn: { type: 'file', filename: 'warn.log' },
+        fileError: { type: 'file', filename: 'error.log' }
+    },
+    categories: {
+        default: { appenders: ["consola"], level: "trace" },
+        info: { appenders: ["consola"], level: "info" },
+        warning: { appenders: ["fileWarn"], level: "warn" },
+        error: { appenders: ["fileError"], level: "error" }
+    }
+});
+
+// SOCKET
+// io.on('connection', async (socket) => {
+//     // Refrescar productos
+//     socket.on('refrescar',(data) => {
+//         io.sockets.emit('productoNuevo',data);
+//     });
+
+//     // Chat de usuarios
+//         try {
+//             const mensajes = {id: "1", mensajes: await Mensaje.find().lean()};
+//             const mensajeSchema = new schema.Entity('mensaje');
+//             const fechaHoraSchema = new schema.Entity('fechaHora');
+//             const authorSchema = new schema.Entity('author',{},{idAttribute: 'mail'});
+//             const mensajesSchema = new schema.Entity('mensajesBase',{
+//                 mensaje: mensajeSchema,
+//                 fechaHora: fechaHoraSchema,
+//                 author: [authorSchema]
+//             });
+//             const mensajesBase = new schema.Entity('mensajes',{
+//                 mensajes: [mensajesSchema]
+//             });
+//             const normalizarMensajes = normalize(mensajes,mensajesBase);
+//             io.sockets.emit('mensajes', normalizarMensajes);
+
+//             socket.on('nuevo', async (data) => {
+//                 const bdMensajes = await Producto.find().lean();
+//                 const bd = new Archivo(bdMensajes);
+//                 await bd.guardarMensaje(data);
+//                 const mensajeSchema = new schema.Entity('mensaje');
+//                 const fechaHoraSchema = new schema.Entity('fechaHora');
+//                 const authorSchema = new schema.Entity('author',{},{idAttribute: 'mail'});
+//                 const mensajesSchema = new schema.Entity('mensajesBase',{
+//                     mensaje: mensajeSchema,
+//                     fechaHora: fechaHoraSchema,
+//                     author: [authorSchema]
+//                 });
+//                 const mensajesBase = new schema.Entity('mensajes',{
+//                     mensajes: [mensajesSchema]
+//                 });
+//                 const normalizarMensajes = normalize(mensajes,mensajesBase);
+//                 io.sockets.emit('mensajes',normalizarMensajes);
+//             });
+//         }
+    
+//         catch(e) {
+//             loggerError.error(e);
+//         }
+// });
